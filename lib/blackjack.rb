756caa079e458a95a2af5fe6d5188d439bc5030d
sgg2123 <sgg2123@columbia.edu>
2018-04-20 04:06:58 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,6 +55,8 @@ def runner
   welcome
   card_total = initial_round
   until card_total > 21
+    if card_total == hit?(card_total)
+      puts "your total was #{card_total}"
     card_total = hit?(card_total)
     display_card_total(card_total)
     
